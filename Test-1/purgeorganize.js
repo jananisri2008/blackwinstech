eecd272852be3d8fe0f5392ca9ac214914e16053
jananisri2008 <110449813+jananisri2008@users.noreply.github.com>
2024-10-22 10:57:36 +0530

    purgeorganize.js

--- Test-1/purgeorganize.js
+++ Test-1/purgeorganize.js
@@ -8,22 +8,22 @@ uniqueSort([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]) ➞ [1, 2, 3, 4]
 uniqueSort([6, 7, 3, 2, 1]) ➞ [1, 2, 3, 6, 7]
  */
 function purgeorganize(arr) {
-       let uniqueArray = [];
+       let array = [];
     for (let i = 0; i < arr.length; i++) {
-        if (!uniqueArray.includes(arr[i])) {
-            uniqueArray.push(arr[i]); 
+        if (!array.includes(arr[i])) {
+            array.push(arr[i]); 
         }
     }
-    for (let i = 0; i < uniqueArray.length; i++) {
-        for (let j = i + 1; j < uniqueArray.length; j++) {
-            if (uniqueArray[i] > uniqueArray[j]) {
-                let temp = uniqueArray[i];
-                uniqueArray[i] = uniqueArray[j];
-                uniqueArray[j] = temp;
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] > array[j]) {
+                let temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
             }
         }
     }
-    return uniqueArray; 
+    return array; 
 }
 console.log(purgeorganize([1, 2, 4, 3]));
 console.log(purgeorganize([1, 4, 4, 4, 4, 4, 3, 2, 1, 2])); 
